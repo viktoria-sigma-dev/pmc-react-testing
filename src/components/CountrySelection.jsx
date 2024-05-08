4a07ca438b67ff45d92284dda56b9f18a89c13bb
Viktoria Shelestenko <viktoria.shelestenko@chronomics.com>
2024-05-08 16:45:30 +0300

    Update country selection

--- src/components/CountrySelection.jsx
+++ src/components/CountrySelection.jsx
@@ -5,6 +5,23 @@ export const CountrySelection = ({ onSave }) => {
   const [selectedState, setSelectedState] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
+  const countryOptions = [
+    { code: 'US', name: 'United States' },
+    { code: 'CA', name: 'Canada' },
+    { code: 'GB', name: 'United Kingdom' },
+    { code: 'AU', name: 'Australia' },
+    { code: 'FR', name: 'France' },
+    { code: 'UA', name: 'Ukraine' }
+  ];
+  const stateOptions = [
+    { code: 'CO', name: 'Colorado' },
+    { code: 'OR', name: 'Oregon' },
+    { code: 'LA', name: 'Los Angeles' },
+    { code: 'NY', name: 'New York' },
+    { code: 'TX', name: 'Texas' },
+  ];
+
+
   const handleCountryChange = (e) => {
     const country = e.target.value;
     setSelectedCountry(country);
@@ -23,7 +40,9 @@ export const CountrySelection = ({ onSave }) => {
     } else if (selectedCountry === 'US' && selectedState === '') {
       setErrorMessage('Please select a state.');
     } else {
-      onSave({ country: selectedCountry, state: selectedState });
+      const countryName = countryOptions.find(country => country.code === selectedCountry)?.name || '';
+      const stateName = stateOptions.find(state => state.code === selectedState)?.name;
+      onSave({ countryCode: selectedCountry, countryName, regionCode: selectedState, regionName: stateName ?? '' });
     }
   };
 
@@ -34,10 +53,9 @@ export const CountrySelection = ({ onSave }) => {
           <h2>Select your country:</h2>
           <select value={selectedCountry} onChange={handleCountryChange}>
             <option value="">Select Country</option>
-            <option value="US">United States</option>
-            <option value="CA">Canada</option>
-            <option value="GB">United Kingdom</option>
-            <option value="AU">Australia</option>
+            {countryOptions.map(country => (
+              <option key={country.code} value={country.code}>{country.name}</option>
+            ))}
           </select>
         </div>
 
@@ -46,9 +64,9 @@ export const CountrySelection = ({ onSave }) => {
             <h2>Select your state:</h2>
             <select value={selectedState} onChange={handleStateChange}>
               <option value="">Select State</option>
-              <option value="CO">Colorado</option>
-              <option value="OR">Oregon</option>
-              <option value="LA">Los Angeles</option>
+              {stateOptions.map(state => (
+                <option key={state.code} value={state.code}>{state.name}</option>
+              ))}
             </select>
           </div>
         )}
